1e552b671f0496eb9ee563fa67c30e270a05e075
godessiris <godessiris@gmail.com>
2019-07-28 23:22:24 -0500

    first commit

--- apps/RS_tvshows_app/models.py
+++ apps/RS_tvshows_app/models.py
@@ -2,16 +2,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+
+
 # Create your models here.
-class BlogManager(models.Manager):
-    def srtv_validator(self, postData):
+class ShowManager(models.Manager):
+    def basic_validator(self,postData):
         errors = {}
-        # add keys and values to errors dictionary for each invalid field
-        if len(postData['name']) < 5:
-            errors["name"] = "Blog name should be at least 5 characters"
-        if len(postData['desc']) < 10:
-            errors["desc"] = "Blog description should be at least 10 characters"
+        
+        if len(postData['title']) == 0:
+            errors["title"] = "Please enter a Show title!"
+        if len(postData['description']) < 5:
+            errors["description"] = "Please, Describe the show with more than 5 words!!"
+        if len(postData['network']) == 0:
+            errors["network"] = "Please enter a network"
+        if len(postData['release_date']) < 10:
+            errors["release_date"] = "Dont forget the date and it should be mm/dd/yyyy!"
         return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -19,6 +26,7 @@ class Show(models.Model):
     network = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects=ShowManager()
 
     def __repr__(self):
         return f"<Show object: {self.title} {self.network} {self.release_date}>"
--- apps/RS_tvshows_app/views.py
+++ apps/RS_tvshows_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
 
 # Create your views here.
+
 def index(request):
     return redirect('/shows')
 
@@ -17,9 +19,15 @@ def new_shows(request):
     
 
 def process_create(request):
-    Show.objects.create(title=request.POST['title'], description=request.POST['description'],release_date=request.POST['release_date'],network=request.POST['network'] )
-    show = Show.objects.last()
-    return redirect(f"/shows/{show.id}")
+    errors=Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request,value)
+        return redirect('/shows/new')
+    else:
+        Show.objects.create(title=request.POST['title'], description=request.POST['description'],release_date=request.POST['release_date'],network=request.POST['network'] )
+        show = Show.objects.last()
+        return redirect(f"/shows/{show.id}")
 
 def read_shows(request, show_id):
     show = Show.objects.get(id=show_id)
@@ -37,19 +45,27 @@ def update_shows(request, show_id):
 
 def process_update(request, show_id):
     show = Show.objects.get(id=show_id)
-    show.title= request.POST["title"]
-    show.network = request.POST["network"]
-    show.description = request.POST["description"]
-    show.release_date= request.POST["release_date"]
-    show.save()
-    context = {
-        'show':show,
-        'show.title': show.title,
-        'show.network':show.network,
-        'show.description':show.description,
-        'show.release_date': show.release_date,
-    }
-    return redirect(f"/shows/{show.id}", context)
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show.id}/edit')
+    else:
+        
+        show.title= request.POST["title"]
+        show.network = request.POST["network"]
+        show.description = request.POST["description"]
+        show.release_date= request.POST["release_date"]
+        show.save()
+        context = {
+            'show':show,
+            'show.title': show.title,
+            'show.network':show.network,
+            'show.description':show.description,
+            'show.release_date': show.release_date,
+        }
+        messages.success(request, "Show successfully updated")
+        return redirect(f"/shows/{show.id}", context)
 
 def process_destroy(request, show_id):
     remove_show=Show.objects.get(id=show_id)
